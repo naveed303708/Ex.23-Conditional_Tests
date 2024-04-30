0eab6d42b425ffbae945f964bf1668a574fea660
Naveed Ahmed Farooqui <162836180+naveed303708@users.noreply.github.com>
2024-05-01 00:01:45 +0500

    Add files via upload

---
+++ conditional_tests.js
@@ -0,0 +1,34 @@
+"use strict";
+// write series of conditional tests
+//creating car variable
+let car = 'subaru';
+// Test No 1
+console.log("Is car == 'subaru'? I predict true.");
+console.log(car === 'subaru');
+// Test No 2
+console.log("Is car == 'subaru'? I predict true.");
+console.log(car == 'subaru');
+// Test No 3
+console.log("Is car != 'Hionda'? I predict true.");
+console.log(car !== 'Honda');
+// Test No 4
+console.log("Is car.lOWER Case !== 'ford'? I predict true.");
+console.log(car !== 'ford');
+// Test No 5
+console.log("Is car.Upper Case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+// Test No 6
+console.log("Is car == 'SUBARU'? I predict false.");
+console.log(car == 'SUBARU');
+// Test No 7
+console.log("Is car === 'Train'? I predict false.");
+console.log(car === 'Train');
+// Test No 8
+console.log("Is car === 'Train'? I predict false.");
+console.log(car === 'Train');
+// Test No 9
+console.log("Is car === 'Bus'? I predict false.");
+console.log(car === 'Bus');
+// Test No 4
+console.log("Is car ==='cycle'? I predict false.");
+console.log(car === 'cycle');
